e7c307d55bbb43d434a56fc13b7e35aeec8b2a15
tamazyanarsen <tamazyanarsen@mail.ru>
2020-04-12 10:44:46 +0300

    компоненты для диалогов и списков

--- src/components/Dialog.vue
+++ src/components/Dialog.vue
@@ -1,5 +1,20 @@
 <template>
-    $END$
+    <div class="dialog">
+        <div class="caption">
+            <div class="title">
+                <slot name="title"/>
+            </div>
+            <div class="close-button">
+                <button class="material-icons"
+                        @click="$emit('close')">close
+                </button>
+            </div>
+        </div>
+        <p class="body">
+            <slot name="form"/>
+            <slot name="confirm"/>
+        </p>
+    </div>
 </template>
 
 <script>
@@ -9,5 +24,35 @@
 </script>
 
 <style scoped>
+    .dialog {
+        position: fixed;
+        background: aliceblue;
+        box-shadow: 0 0 100px rgba(0, 0, 0, 0.5); /* Параметры тени */
+        opacity: 1;
+        left: 10%;
+        top: 10%;
+        z-index: 1000;
+        height: 80vh;
+        width: 80%;
+    }
 
+    .caption {
+        width: 100%;
+        height: 10%;
+    }
+
+    .title {
+        float: left;
+        padding-left: 1%;
+    }
+
+    .body{
+        margin: 1%;
+    }
+
+    .close-button {
+        float: right;
+        padding-right: 1%;
+        padding-top: 1%;
+    }
 </style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,10 +1,19 @@
 <template>
-    $END$
+    <div>
+        <div v-for="item in items"
+             :key="item.id">
+            <slot name="item-template"
+                  :item="item"/>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "List"
+        name: "List",
+        props: {
+            items: Array
+        }
     }
 </script>
 
