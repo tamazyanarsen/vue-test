81d1e7098f01cc092e0ba1734f4ea72fa726d062
tamazyanarsen <tamazyanarsen@mail.ru>
2020-04-13 08:12:21 +0300

    удалил лишний код

--- src/storage/NotesData.js
+++ src/storage/NotesData.js
@@ -1,9 +1,1 @@
-import { Note } from "@/models/Note";
-import { Todo } from "@/models/Todo";
-
-export const notes = JSON.parse(localStorage.getItem('notes')) ||
-    [
-        new Note('заметка 1', [new Todo('task 1'), new Todo('task 2', true)]),
-        new Note('заметка 2', [new Todo('task 1'), new Todo('task 2', true),
-            new Todo('task 2')])
-    ];
+export const notes = JSON.parse(localStorage.getItem('notes')) || [];
